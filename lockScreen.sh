c1f93e5d509808cf313bec3dcfc49b9a1b25ca9f
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2017-12-03 23:17:55 +0330

    Add LockScreen.sh

---
+++ lockScreen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Take Screenshot
+scrot -b 'lockScreen.jpg'
+# convert to png and blur
+convert lockScreen.jpg -blur 15x5 blurred.png
+# put on-image on blurred.png
+composite -gravity center /home/ali/src/lockScreen/on-image.png blurred.png final.png
+
+# Remove tmp files and add final image to tmp
+rm blurred.png lockScreen.jpg
+rm /tmp/lockScreen.png
+cp final.png /tmp/lockScreen.png
+rm final.png
+# Run i3lock
+i3lock -i /tmp/lockScreen.png -d -I 30
